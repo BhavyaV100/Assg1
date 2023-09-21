9146ed258e45e99715f36c38fce5768aa0fb90a4
Bhavya_55 <bhavyavaishnav55@gmail.com>
2023-09-21 19:56:41 -0400

    Assignment for today

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func BhavyaAdd(a, b int) int {
+    return a + b
+}
+
+func main() {
+    result := BhavyaAdd(2, 2)
+    fmt.Printf("Bhavya function add: %d\n", result)
+}
