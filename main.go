58749583eaa0a8dcc159f38466d3cfd025ca6a6c
JEEL <Jeeldungarani7@gmail.com>
2023-09-21 20:30:11 -0400

    jeel's function have been added

--- main.go
+++ main.go
@@ -3,10 +3,25 @@ package main
 import "fmt"
 
 func BhavyaAdd(a, b int) int {
-    return a + b
+	return a + b
+}
+
+func printTPattern(height int) {
+	height = 7
+	for i := 0; i < height; i++ {
+		for j := 0; j < height; j++ {
+			if i == 0 || j == height/2 {
+				fmt.Print("*")
+			} else {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
+	}
 }
 
 func main() {
-    result := BhavyaAdd(2, 2)
-    fmt.Printf("Bhavya function add: %d\n", result)
+	result := BhavyaAdd(2, 2)
+	fmt.Printf("Bhavya function add: %d\n", result)
+	printTPattern(7)
 }
