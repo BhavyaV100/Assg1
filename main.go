780fd9f8093b982fff67b90037a270a272d5f0df
sahilpatel17 <sahilofficialpatel@gmail.com>
2023-09-21 20:57:55 -0400

    Sahil

--- main.go
+++ main.go
@@ -31,6 +31,12 @@ func (d *Bikes) sent() {
 	fmt.Printf("%s is an excellent bike which has %s tires. The owner's name is %s who bought it with %s\n", d.Name, d.Type, d.Owner, d.Price)
 }
 
+func sahiltrianglearea(length, width int) int {
+
+	Ar := 0.5 * length * width
+	return Ar
+}
+
 func main() {
 	Honda := Bikes{
 		Name:  "R15",
@@ -43,4 +49,5 @@ func main() {
 	result := BhavyaAdd(2, 2)
 	fmt.Printf("Bhavya function add: %d\n", result)
 	printTPattern(7)
+	fmt.Printf("Area of triangle by Sahil : %d", sahiltrianglearea(12, 10))
 }
