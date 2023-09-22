4f7c286aae66677ca991ccc396f8d8105a45dc89
DarshanGBhut <darshanbhut654@gmail.com>
2023-09-21 20:50:09 -0400

    Darshan

--- main.go
+++ main.go
@@ -20,7 +20,26 @@ func printTPattern(height int) {
 	}
 }
 
+type Bikes struct {
+	Name  string
+	Type  string
+	Owner string
+	Price string
+}
+
+func (d *Bikes) sent() {
+	fmt.Printf("%s is an excellent bike which has %s tires. The owner's name is %s who bought it with %s\n", d.Name, d.Type, d.Owner, d.Price)
+}
+
 func main() {
+	Honda := Bikes{
+		Name:  "R15",
+		Type:  "2",
+		Owner: "Darshan",
+		Price: "$125,000",
+	}
+
+	Honda.sent()
 	result := BhavyaAdd(2, 2)
 	fmt.Printf("Bhavya function add: %d\n", result)
 	printTPattern(7)
